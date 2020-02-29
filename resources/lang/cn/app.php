760d4516e2fe8d51f1e08b0802b568542eececc3
inerversleep <43922796+inerversleep@users.noreply.github.com>
2020-02-29 15:10:17 +0800

    Create app.php

---
+++ resources/lang/cn/app.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => '系统',
+    'settings.appearance' => '界面',
+    'settings.miscellaneous' => '杂项',
+    'settings.support' => '支持',
+    'settings.donate' => '捐赠',
+
+    'settings.version' => '版本',
+    'settings.background_image' => '背景图片',
+    'settings.window_target' => '链接打开方式',
+    'settings.window_target.current' => '在当前标签打开',
+    'settings.window_target.one' => '在同一个标签内打开',
+    'settings.window_target.new' => '在新标签打开',
+    'settings.homepage_search' => '主页搜索框',
+    'settings.search_provider' => '默认搜索引擎',
+    'settings.language' => '语言',
+    'settings.reset' => '恢复默认设置',
+    'settings.remove' => '移除',
+    'settings.search' => '搜索',
+    'settings.no_items' => '未找到任何项目',
+
+
+    'settings.label' => '项目',
+    'settings.value' => '值',
+    'settings.edit' => '编辑',
+    'settings.view' => '查看',
+
+    'options.none' => '- 未设置 -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'StartPage',
+    'options.yes' => '是',
+    'options.no' => '否',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => '保存',
+    'buttons.cancel' => '取消',
+    'buttons.add' => '增加',
+    'buttons.upload' => '上传图标',
+    'buttons.downloadapps' => '更新应用列表',
+
+    'dash.pin_item' => '固定到导航页',
+    'dash.no_apps' => '目前没有项目固定在导航页, :link1 or :link2',
+    'dash.link1' => '增加一个项目到这里',
+    'dash.link2' => '将一个项目固定到这里',
+    'dash.pinned_items' => '固定项目',
+
+    'apps.app_list' => '项目列表',
+    'apps.view_trash' => '查看回收站',
+    'apps.add_application' => '增加项目',
+    'apps.application_name' => '项目名',
+    'apps.colour' => '背景色',
+    'apps.icon' => '图标',
+    'apps.pinned' => '固定',
+    'apps.title' => '标题',
+    'apps.hex' => '16进制颜色',
+    'apps.username' => '用户名',
+    'apps.password' => '密码',
+    'apps.config' => '设置',
+    'apps.apikey' => 'API密钥',
+    'apps.enable' => '启用',
+    'apps.tag_list' => '标签列表',
+    'apps.add_tag' => '增加标签',
+    'apps.tag_name' => '标签名',
+    'apps.tags' => '标签',
+    'apps.override' => '如果与主网址不同',
+    'apps.preview' => '预览',
+    'apps.apptype' => '应用图标',
+
+    'dashboard' => '导航页面',
+
+    'user.user_list' => '用户',
+    'user.add_user' => '增加用户',
+    'user.username' => '用户名',
+    'user.avatar' => '头像',
+    'user.email' => 'Email',
+    'user.password_confirm' => '确认密码',
+    'user.secure_front' => '允许任意人进入前台 - 设置密码后才可启用',
+    'user.autologin' => '允许使用特定的网址登陆。任何知道链接的人都可以登录。',
+
+    'url' => '网址',
+    'title' => '标题',
+    'delete' => '删除',
+    'optional' => '选项',
+    'restore' => '恢复',
+
+    'alert.success.item_created' => '项目创建成功',
+    'alert.success.item_updated' => '项目更新成功',
+    'alert.success.item_deleted' => '项目删除成功',
+    'alert.success.item_restored' => '项目恢复成功',
+    'alert.success.updating' => '更新项目列表',
+
+    'alert.success.tag_created' => '标签创建成功',
+    'alert.success.tag_updated' => '标签更新成功',
+    'alert.success.tag_deleted' => '标签删除成功',
+    'alert.success.tag_restored' => '标签恢复成功',
+
+    'alert.success.setting_updated' => '已设置成功',
+    'alert.error.not_exist' => '此设置不存在',
+
+    'alert.success.user_created' => '用户创建成功',
+    'alert.success.user_updated' => '用户更新成功',
+    'alert.success.user_deleted' => '用户删除成功',
+    'alert.success.user_restored' => '用户恢复成功',
+
+
+];

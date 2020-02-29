feb1911351f28e39398214549efb268eda21f88b
inerversleep <43922796+inerversleep@users.noreply.github.com>
2020-02-29 16:12:42 +0800

    Create passwords.php

---
+++ resources/lang/cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少包含六个字符，并且与密码确认匹配。',
+    'reset' => '密码已重置',
+    'sent' => '我们已经通过电子邮件发送了您的密码重置链接。',
+    'token' => '此密码重置令牌无效。',
+    'user' => "没用使用该电子邮件地址的用户。",
+
+];
